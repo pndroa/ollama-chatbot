f5c8a89a1069e785d87f9bd1a3bab5be2361d906
pndroa <157326278+pndroa@users.noreply.github.com>
2026-05-01 21:06:16 +0200

    feat: create test api and integration with the frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,31 @@
+"use client"
+import {useState, useEffect} from "react"
+import axios from "axios"
+
+
 export default function Home() {
-  return <div>Hello World</div>
+  
+  const [ollamaModels, setOllamaModels] = useState<string[]>([])
+  const [error, setError] = useState<string | null>(null)
+  
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/v1/models").then((reponse) => {
+      setOllamaModels(reponse.data.models)
+    }).catch((err) => {
+      setError(err.message)
+    })
+  }, [])
+  
+  if(error) return <div>Error: {error}</div>
+  
+  return (
+    <div>
+      <h1>Ollama Models</h1>
+      <ul>
+        {ollamaModels.map((model) => (
+            <li key={model}>{model}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
